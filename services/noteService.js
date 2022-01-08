b71b8e4b58ed3c2788b8cc1d417cb9b8aaf58e5c
Igor Zashchelkin <igorzash@tutanota.com>
2022-01-08 18:02:58 +0200

    Dates parser

--- services/noteService.js
+++ services/noteService.js
@@ -9,12 +9,18 @@ class NoteService {
 		this.repository.createNote(contents, category);
 	}
 
+	_parse_dates(note) {
+		note.dates = note.contents.match(/\d{1,2}\/\d{1,2}\/\d{4}/g) || [];
+
+		return note;
+	}
+
 	getNote(id) {
-		return this.repository.getNote(id);
+		return this._parse_dates(this.repository.getNote(id));
 	}
 
 	getAllNotes() {
-		return this.repository.getAllNotes();
+		return this.repository.getAllNotes().map(this._parse_dates);
 	}
 
 	deleteNote(id) {
